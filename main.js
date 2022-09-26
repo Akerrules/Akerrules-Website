47ebd91aebc2eeed6e37e9f3d455dede9444691f
Akerrules <addrido@gmail.com>
2022-09-26 05:07:37 -0400

    work on the main page
    added new logo and created sub container for information

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  bannerDots();
+}, false);
+
 function bannerDots(){
-    const dotElem = document.getElementById('dots');
-    const parentElem = document.getElementById('banner-background')
-    for(let i = 0; i<20; i++){
-        let clone = dotElem.cloneNode();
-        parentElem.appendChild(clone)
+    var c = document.getElementById("myCanvas");
+     let ctx =  c.getContext("2d");
+    let maxHeight = c.getBoundingClientRect().height;
+    let maxWidth = c.getBoundingClientRect().width;
+    console.log(maxHeight)
+    let x;
+    let y;
+    
+    //const dotElem = document.getElementById('dots');
+  //  const parentElem = document.getElementById('banner-background')
+    for(let i = 0; i<maxWidth/10; i++){
+        x = Math.floor(Math.random() * maxWidth);
+
+        y = Math.floor(Math.random() * maxHeight);
+
+      
+        ctx.beginPath();
+      
+        ctx.arc(x, y, 5, 0,2*Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    //    let clone = dotElem.cloneNode(true);
+    //    clone.id
+    //    parentElem.appendChild(clone)
     }
 }
